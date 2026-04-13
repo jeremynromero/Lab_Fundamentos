a28110a3f760e3916dbc14b1d0e3adfb2f011582
jeremynromero <jeremyn20171@gmail.com>
2026-04-13 01:09:08 -0500

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+# Fase 1: Promedio Simple de 5 notas
+print("--- Calculadora de Promedios V1 ---")
+
+# Entrada de datos
+=======
+# Versión 1: Promedio Simple de 5 notas
+print("--- Calculadora de Promedios V1 ---")
+>>>>>>> e38ce87 (V1: Algoritmo secuencial)
+nota1 = float(input("Ingrese nota 1: "))
+nota2 = float(input("Ingrese nota 2: "))
+nota3 = float(input("Ingrese nota 3: "))
+nota4 = float(input("Ingrese nota 4: "))
+nota5 = float(input("Ingrese nota 5: "))
+<<<<<<< HEAD
+
+# Proceso
+promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+
+# Salida
+=======
+promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+>>>>>>> e38ce87 (V1: Algoritmo secuencial)
+print(f"El promedio simple es: {promedio}")
