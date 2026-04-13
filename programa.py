df4297a04dcead8ab700cdaf194a44735268d582
Jeremyn <jeremyn20171@gmail.com>
2026-04-13 00:35:07 -0500

    V3: Agregada validación condicional de aprobación

--- programa.py
+++ programa.py
@@ -6,3 +6,7 @@ nota3 = float(input("Ingrese nota 3 (30%): "))
 nota4 = float(input("Ingrese nota 4 (40%): "))
 promedio = (nota1 * 0.10) + (nota2 * 0.20) + (nota3 * 0.30) + (nota4 * 0.40)
 print(f"El promedio ponderado es: {promedio}")
+if promedio >= 12:
+    print ("Estado: ¡Aprobado! ")
+else:
+    print("Estado: Reprobado ")
